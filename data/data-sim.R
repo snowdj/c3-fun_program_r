68ad27cde3d34ab880d6fe43d097d0d23a16349c
Daniel Anderson <daniela@uoregon.edu>
2019-04-23 14:00:59 -0700

    re-simulated data with disability codes as a factor

--- data/data-sim.R
+++ data/data-sim.R
@@ -3,6 +3,10 @@ library(tidyverse)
 library(fs)
 
 l <- map(dir_ls(here::here("data", "pfiles18")), read_csv)
+l <- map(l, ~mutate(., 
+                    asmtprmrydsbltycd = as.factor(asmtprmrydsbltycd),
+                    asmtscndrydsbltycd = as.factor(asmtscndrydsbltycd)))
+  
 sim <- map(l, syn)
 sim <- map(sim, "syn")
 sim <- map(sim, ~.x[!names(.x) == "enrlgrattst"])
